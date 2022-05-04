11a8da508816de46e375bb907563fdf3f16853b8
Thomas Andrew Martin <47917727+Dev0dyssey@users.noreply.github.com>
2022-05-04 09:26:23 +0100

    Added randomisation to dice rolls in App

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,21 +13,26 @@ class ViewController: UIViewController {
     @IBOutlet var diceImageView1: UIImageView!
     @IBOutlet var diceImageView2: UIImageView!
     
+    let diceArr = [
+        UIImage(named: "DiceOne"),
+        UIImage(named: "DiceTwo"),
+        UIImage(named: "DiceThree"),
+        UIImage(named: "DiceFour"),
+        UIImage(named: "DiceFive"),
+        UIImage(named: "DiceSix"),
+    ]
+    
+    // Code block no longer needed, can be deleted
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        diceImageView1.image = UIImage(named: "DiceThree")
-        diceImageView1.alpha = 0.5
-        
-        diceImageView2.image = UIImage(named: "DiceTwo")
         // Do any additional setup after loading the view.
     }
 
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Button got tapped")
-        diceImageView1.image = UIImage(named: "DiceFive")
-        diceImageView2.image = UIImage(named: "DiceThree")
+        diceImageView1.image = diceArr[Int.random(in: 0...5)]
+        diceImageView2.image = diceArr[Int.random(in: 0...5)]
     }
 }
 
